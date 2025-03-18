74078c4c3bbc3a683172bd5737268af3ac993cef
Svetlana Polyanskaya <s.s.polyanskaya@gmail.com>
2025-03-18 14:49:52 +0300

    Add comments

--- seqClass.py
+++ seqClass.py
@@ -4,33 +4,63 @@
 import sys, re
 from argparse import ArgumentParser
 
-#parse arguments
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+parser.add_argument("-p", "--percentage", action="store_true", help="Enable nucleotide percentage calculation")
 
-#check the arguments
+#parse arguments
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
+args.seq = args.seq.upper()                 
+
+
+DNA = False
+RNA = False
+
+#Check if seq is DNA or RNA
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+    if re.search('T', args.seq) and not re.search('U', args.seq):
         print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+        DNA = True
+    elif re.search('U', args.seq) and not re.search('T', args.seq):
         print ('The sequence is RNA')
+        RNA = True
+    elif re.search('T', args.seq) and re.search('U', args.seq):
+        print('The sequence is not DNA nor RNA')
     else:
         print ('The sequence can be DNA or RNA')
+        DNA = True
+        RNA = True
 else:
     print ('The sequence is not DNA nor RNA')
-
+    
+# Motif search
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("FOUND! YEP!")
+        print("FOUND") # if motif is found print FOUND
     else:
-        print("NOT FOUND... bruh...")
+        print("NOT FOUND") # if motif is not found print NOT FOUND
+        
+# Percentage calculation
+if args.percentage:
+    nucleotides = {}
+    for letter in set(args.seq):
+        nucleotides[letter] = 0
+    if DNA == True or RNA == True:
+        for i in args.seq:
+    	    for k in nucleotides.keys():
+    	        if i == k:
+    	            nucleotides[k] += 1
+        print(f'\nNucleotide percentage:')  
+        for key in nucleotides:
+            print(f'{key}\t{round(nucleotides[key]/len(args.seq), 4)*100}')
+    else:
+        print('Percentage calculation is non-applicable.')
+        

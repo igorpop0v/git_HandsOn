92ad6660d682b036b1e604e388d84b5661b3a8a7
igorpop0v <doc.igor.popov@gmail.com>
2025-03-18 13:37:41 +0300

    add -m argument

--- seqClass.py
+++ seqClass.py
@@ -3,8 +3,9 @@
 import sys, re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA - works for upper and lower cases!')
+parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -12,6 +13,7 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
+args.seq = args.seq.upper()                 # Note we just added this line
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -21,3 +23,11 @@ if re.search('^[ACGTU]+$', args.seq):
         print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
+
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
